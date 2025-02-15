852365dd0ca30844aef468f2fd6ba1b86b507a31
alexc83 <alex.cruce@gmail.com>
2025-02-15 02:58:08 -0600

    Section complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  //template: `<p>Hello World!</p>`,
+  standalone: true,
+  templateUrl:'./app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-basics';
+
 }
